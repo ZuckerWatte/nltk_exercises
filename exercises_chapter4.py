fda61914a66fffa75d71ecabe9bafb24a39b2272
Mona Fenzl <oranges_gummibearle@web.de>
2017-05-13 16:05:39 +0200

    exercise 4.2

---
+++ exercises_chapter4.py
@@ -0,0 +1,32 @@
+import nltk, re, pprint
+
+# #######################
+# # 4.2 Tuples and Lists
+# #######################
+
+# def sequence_operations(list_or_tuple):
+#     print('Sequenz:', list_or_tuple)
+#     print('Sortierte Sequenz:', sorted(list_or_tuple))
+#     print('Umgekehrte Reihenfolge', [x for x in reversed(list_or_tuple)])
+#     print('Sequenz hat die Länge:', len(list_or_tuple))
+#     print('Vereinen zweier Sequenzen', list_or_tuple + list_or_tuple)
+#     print('Multiplizieren einer Sequenz:', list_or_tuple * 3)
+#     print('4 appears', list.count(4), 'times')
+#
+#
+# list = [4, 8, 2]
+# tuple = (4, 8, 2)
+# sequence_operations(list)
+# sequence_operations(tuple)
+
+# def list_operations(list):
+#     list.append(6)
+#     print('Element was adden:', list)
+#     list.pop()
+#     print('Last element was removed:', list)
+#     list.remove(8)
+#     print('Specific elment was removed:', list)
+#
+# list = [4, 8, 2]
+# list_operations(list)
+# #######################
