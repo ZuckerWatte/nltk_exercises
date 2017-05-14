3485c19538dc42001b51b40d28bd2402e0a3ac28
Mona Fenzl <oranges_gummibearle@web.de>
2017-05-14 17:21:12 +0200

    exercise 4.17

--- exercises_chapter4.py
+++ exercises_chapter4.py
@@ -101,64 +101,87 @@ import nltk, re, pprint
 # # 4.16 Gematria
 # ################
 
-import random
+# import random
+#
+# letter_vals = {
+#     'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 80, 'g': 3,
+#     'h': 8, 'i':10, 'j':10, 'k':20, 'l':30, 'm':40, 'n':50,
+#     'o':70, 'p':80, 'q':100, 'r':200, 's':300, 't':400, 'u':6,
+#     'v':6, 'w':800, 'x':60, 'y':10, 'z':7
+# }
+#
+# def gematria(word):
+#     gematria_sum = sum(v for v in [letter_vals[c.lower()] for c in word])
+#     return gematria_sum
+#
+# def find_666(text):
+#     gematria_vals = [(w, gematria(w)) for w in text if re.search(r'^[a-zA-Z]+$', w)]
+#     count_666 = [(w, val) for (w, val) in gematria_vals if val == 666]
+#     return len(count_666), count_666
+#
+# def process_documents(files, corpus):
+#     for file in files:
+#         count_666 = find_666(corpus.words(file))
+#         if count_666[0] is not 0:
+#             print('\n The document {} contains {} words with the gematria number 666: {}'.format(file, count_666[0], [w for (w, _) in count_666[1]]))
+#         else:
+#             print('\n The document {} contains NO words with the gematria number 666.'.format(file))
+#
+# def built_gematria_lexicon(gematria_words):
+#     gematria_lexicon = {}
+#     for w, val in gematria_words:
+#         if re.search(r'^[a-zA-Z]+$', w):
+#             if val in gematria_lexicon:
+#                 gematria_lexicon[val].append(w)
+#             else:
+#                 gematria_lexicon[val] = [w]
+#     return gematria_lexicon
+#
+#
+# def decode(text, gematria_lexicon, percentage=0.3):
+#     replace_percentage = int(percentage * len(text))
+#     replace_positions = [random.choice(range(len(text))) for _ in range(replace_percentage)]
+#     gematria_vals = [(pos, gematria(text[pos])) for pos in replace_positions]
+#     for pos, gem in gematria_vals:
+#         if gem in gematria_lexicon:
+#             gematria_equivalent = random.choice(gematria_lexicon[gem])
+#             text[pos] = gematria_equivalent
+#     hidden_meaning = ' '.join(text)
+#     return hidden_meaning
+#
+# # find occurences of words with gematria number 666
+# state_union_corpus = nltk.corpus.state_union
+# state_union_files = nltk.corpus.state_union.fileids()
+# #process_documents(state_union_files, state_union_corpus)
+#
+# # replace randomly words with gematria equivalent
+# brown_words = set(nltk.corpus.brown.words())
+# brown_words_gematria = [(w, gematria(w)) for w in brown_words if re.search(r'^[a-zA-Z]+$', w)]
+# brown_gematria_lexicon = built_gematria_lexicon(brown_words_gematria)
+#
+# text = nltk.word_tokenize(input('\n Please enter some text: '))
+# text = [t for t in text if re.search(r'^[a-zA-Z]+$', t)]
+# print('\n The hiffen meaning of your text is: ', decode(text, brown_gematria_lexicon))
 
-letter_vals = {
-    'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 80, 'g': 3,
-    'h': 8, 'i':10, 'j':10, 'k':20, 'l':30, 'm':40, 'n':50,
-    'o':70, 'p':80, 'q':100, 'r':200, 's':300, 't':400, 'u':6,
-    'v':6, 'w':800, 'x':60, 'y':10, 'z':7
-}
-
-def gematria(word):
-    gematria_sum = sum(v for v in [letter_vals[c.lower()] for c in word])
-    return gematria_sum
-
-def find_666(text):
-    gematria_vals = [(w, gematria(w)) for w in text if re.search(r'^[a-zA-Z]+$', w)]
-    count_666 = [(w, val) for (w, val) in gematria_vals if val == 666]
-    return len(count_666), count_666
-
-def process_documents(files, corpus):
-    for file in files:
-        count_666 = find_666(corpus.words(file))
-        if count_666[0] is not 0:
-            print('\n The document {} contains {} words with the gematria number 666: {}'.format(file, count_666[0], [w for (w, _) in count_666[1]]))
-        else:
-            print('\n The document {} contains NO words with the gematria number 666.'.format(file))
-
-def built_gematria_lexicon(gematria_words):
-    gematria_lexicon = {}
-    for w, val in gematria_words:
-        if re.search(r'^[a-zA-Z]+$', w):
-            if val in gematria_lexicon:
-                gematria_lexicon[val].append(w)
-            else:
-                gematria_lexicon[val] = [w]
-    return gematria_lexicon
+# ################
 
 
-def decode(text, gematria_lexicon, percentage=0.3):
-    replace_percentage = int(percentage * len(text))
-    replace_positions = [random.choice(range(len(text))) for _ in range(replace_percentage)]
-    gematria_vals = [(pos, gematria(text[pos])) for pos in replace_positions]
-    for pos, gem in gematria_vals:
-        if gem in gematria_lexicon:
-            gematria_equivalent = random.choice(gematria_lexicon[gem])
-            text[pos] = gematria_equivalent
-    hidden_meaning = ' '.join(text)
-    return hidden_meaning
+#######################################
+# 4.17 Remove frequently occuring words
+#######################################
 
-# find occurences of words with gematria number 666
-state_union_corpus = nltk.corpus.state_union
-state_union_files = nltk.corpus.state_union.fileids()
-#process_documents(state_union_files, state_union_corpus)
+def shorten(text, n):
+    tokens = nltk.word_tokenize(text)
+    most_common_words = nltk.FreqDist([t.lower() for t in tokens]).most_common(n)
+    shortened_tokens = []
+    for i, t in enumerate(tokens):
+        if not t.lower() in [w for (w, _) in most_common_words]:
+            shortened_tokens.append(t)
 
-# replace randomly words with gematria equivalent
-brown_words = set(nltk.corpus.brown.words())
-brown_words_gematria = [(w, gematria(w)) for w in brown_words if re.search(r'^[a-zA-Z]+$', w)]
-brown_gematria_lexicon = built_gematria_lexicon(brown_words_gematria)
+    shortened_text = ' '.join(shortened_tokens)
+    return shortened_text
 
-text = nltk.word_tokenize(input('\n Please enter some text: '))
-text = [t for t in text if re.search(r'^[a-zA-Z]+$', t)]
-print('\n The hiffen meaning of your text is: ', decode(text, brown_gematria_lexicon))
+
+text = input('\n  Please enter some text: ')
+print(shorten(text, 3))
+#######################################
