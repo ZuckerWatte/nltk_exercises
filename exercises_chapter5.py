013b374e4cf8c0114e58591d17823c7b1eb79674
Mona Fenzl <oranges_gummibearle@web.de>
2017-06-02 12:57:28 +0200

    chapter 5, exercises 11, 13, 15

---
+++ exercises_chapter5.py
@@ -0,0 +1,61 @@
+import nltk, re, pprint
+from nltk.corpus import brown
+
+# #######################
+# # 5.11 Affix Tagger
+# #######################
+
+# help(nltk.AffixTagger)
+# brown_tagged_sents = brown.tagged_sents(categories=['news', 'hobbies'])
+# brown_sents = brown.sents(categories=['news', 'hobbies'])
+# size = int(len(brown_tagged_sents) * 0.9)
+# train_sents = brown_tagged_sents[:size]
+# test_sents = brown_tagged_sents[size:]
+#
+# bigram_tagger = nltk.BigramTagger(train_sents)
+# affix_tagger = nltk.AffixTagger(train_sents, affix_length=-3, min_stem_length=1, backoff=bigram_tagger)
+#
+# print(affix_tagger.tag_sents(brown_sents[0:10]))
+# print(bigram_tagger.evaluate(test_sents))
+# print(affix_tagger.evaluate(test_sents))
+
+# #######################
+
+
+# ###############################################
+# # 5.13 String Formatting with dictonary values
+# ###############################################
+# from datetime import date
+#
+# date = {'day': date.today().day,
+#         'month': date.today().month,
+#         'year': date.today().year}
+# print('Today is the {day}.{month}.{year}'.format(**date))
+# print('Today is the {year}-{month}-{day}'.format(**date))
+
+# ###############################################
+
+
+# ###############################################
+# # 5.15 String Formatting with dictonary values
+# ###############################################
+
+brown_tagged_words = brown.tagged_words()
+singular_nouns = [n[0] for n in brown_tagged_words if n[1] == 'NN']
+plural_nouns = [n[0] for n in brown_tagged_words if n[1] == 'NNS']
+
+cfdist = nltk.ConditionalFreqDist()
+
+for n in singular_nouns:
+    condition = 'singular'
+    cfdist[condition][n] += 1
+
+for n in plural_nouns:
+    condition = 'plural'
+    cfdist[condition][n[0:-1]] += 1
+
+more_plural = list(n for n in set(singular_nouns + plural_nouns) if cfdist['plural'][n] > cfdist['singular'][n])
+tuples = list((m+'s', cfdist['plural'][m], m, cfdist['singular'][m]) for m in more_plural)
+print(sorted(tuples, key=lambda m:m[3], reverse=True))
+
+# ###############################################
